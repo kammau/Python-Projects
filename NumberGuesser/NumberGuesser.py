3dfdbe4aa10949a06af4e2114e842fdd9c3545b9
Kamryn Smith <programersign@gmail.com>
2026-05-29 14:44:19 -0700

    Removed main function and used while loop instead

--- NumberGuesser/NumberGuesser.py
+++ NumberGuesser/NumberGuesser.py
@@ -1,32 +1,39 @@
+# For random number generation between set range
 from random import randint
 
-
 print("--- Welcome to the Number Guessing Game! ---\n")
-main()
-#rename funcs?
+continue_game = "y"
 
-def main():
-    lowerBound = int(input("Please enter a lower bound: "))
-    upperBound = int(input("Please enter an upper bound: "))
+while (continue_game == "y"):
+    try:
+        lowerBound = int(input("Please enter a lower bound: "))
+        upperBound = int(input("Please enter an upper bound: "))
+        num = randint(lowerBound, upperBound)
+    except ValueError:
+        print("\n***That is not a valid number!***\n")
+        continue
 
-    num = randint(lowerBound, upperBound)
     guess_num = 0
     guess = False
 
-    print("You'll have 5 chances to guess the correct number!\n")
+    print("\nYou'll have 5 chances to guess the correct number!\n")
+    #FIXME: Delete later
+    print(f"Correct num: {num}")
     print("Ready...\nSet...\nGo!\n")
 
     while ((guess == False) & (guess_num < 5)):
         guess_num += 1
-        user_guess = input(f"Guess {guess_num}: ")
+        user_guess = int(input(f"Guess {guess_num}: "))
 
         if (user_guess == num):
             print("Congratulations, you've won!")
+            break
     
-    user_response = input("Would you like to play again (y/n)?")
+    user_response = input("\nWould you like to play again (y/n)? ")
 
     if (user_response == "y"):
-        main()
+        continue
     else:
         print("Thanks for playing!")
-    
+        continue_game = "n"
+        break
