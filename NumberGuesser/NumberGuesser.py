3a5fcd46a2cec677cc572911d76065036d94062e
Kamryn Smith <programersign@gmail.com>
2026-05-30 21:27:15 -0700

    Added input validation for num guess + win loose remarks

--- NumberGuesser/NumberGuesser.py
+++ NumberGuesser/NumberGuesser.py
@@ -13,7 +13,7 @@ while (continue_game == "y"):
         print("\n***That is not a valid number!***\n")
         continue
 
-    guess_num = 0
+    guess_num = 1
     guess = False
 
     print("\nYou'll have 5 chances to guess the correct number!\n")
@@ -21,14 +21,25 @@ while (continue_game == "y"):
     print(f"Correct num: {num}")
     print("Ready...\nSet...\nGo!\n")
 
-    while ((guess == False) & (guess_num < 5)):
-        guess_num += 1
-        user_guess = int(input(f"Guess {guess_num}: "))
+    while ((guess == False) & (guess_num <= 5)):
+        try:
+            user_guess = int(input(f"Guess {guess_num}: "))
+        except ValueError:
+            print("\n***That is not a valid number!***\n")
+            continue
 
         if (user_guess == num):
             print("Congratulations, you've won!")
+            guess = True
             break
-    
+        
+        guess_num += 1
+
+    if (guess == False):
+        print("Better luck next time!")
+    else:
+        print("Your a natural!")
+        
     user_response = input("\nWould you like to play again (y/n)? ")
 
     if (user_response == "y"):
