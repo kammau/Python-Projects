f53edd7ffd01f4467a4554f8d67d4f83e82b5395
Kamryn Smith <programersign@gmail.com>
2026-08-18 17:48:46 -0700

    Got rid of unneeded for loop

--- WordGuesser/WordGuesser.py
+++ WordGuesser/WordGuesser.py
@@ -1,6 +1,6 @@
 """ Author: Kamryn Smith
     Date Completed: 
-    Time Worked On (Including idea development and refinement): 30 min
+    Time Worked On (Including idea development and refinement): 45 min
 
     Program Description: 
 """
@@ -9,7 +9,21 @@ import random
 
 # 10 random words that I generated from the internet LOL
 words = ["Pencil", "Ocean", "Guitar", "Cloud", "Window", "Apple", "Tiger", "House", "River", "Book"]
+target_word = list(random.choice(words).lower())
 
-target_word = random.choice(words)
+player_name = input("What is your name player? ")
+print(f"Welcome {player_name}! You will have 10 trys to guess the word good luck!\n")
+
 
 print(target_word)
+for letter in target_word:
+    print("_", end=" ")
+
+
+user_input = input("\n\nGuess a letter: ").lower()
+
+if (user_input in target_word):
+    print("Yes")
+
+else:
+    print ("\n Wrong! \n")
