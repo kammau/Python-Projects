70be216a8315c7432328b688ad38c9d7fa9fa5c1
Kamryn Smith <programersign@gmail.com>
2026-08-19 20:00:12 -0700

    created list to hold guessed letter undercoverings

--- WordGuesser/WordGuesser.py
+++ WordGuesser/WordGuesser.py
@@ -10,14 +10,16 @@ import random
 # 10 random words that I generated from the internet LOL
 words = ["Pencil", "Ocean", "Guitar", "Cloud", "Window", "Apple", "Tiger", "House", "River", "Book"]
 target_word = list(random.choice(words).lower())
+guessed_letters = ["_" for char in target_word]
 
 player_name = input("What is your name player? ")
 print(f"Welcome {player_name}! You will have 10 trys to guess the word good luck!\n")
 
+i = 0
 
 print(target_word)
-for letter in target_word:
-    print("_", end=" ")
+for letter in guessed_letters:
+    print(letter, end=" ")
 
 
 user_input = input("\n\nGuess a letter: ").lower()
