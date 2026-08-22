172a651d6ed25d3e5172e722d8143f005e36f789
Kamryn Smith <programersign@gmail.com>
2026-08-21 18:22:49 -0700

    Created while loop to hold main game functionality

--- WordGuesser/WordGuesser.py
+++ WordGuesser/WordGuesser.py
@@ -11,26 +11,35 @@ import random
 words = ["Pencil", "Ocean", "Guitar", "Cloud", "Window", "Apple", "Tiger", "House", "River", "Book"]
 target_word = list(random.choice(words).lower())
 guessed_letters = ["_" for char in target_word]
+attempts = 0
 
 player_name = input("What is your name player? ")
 print(f"Welcome {player_name}! You will have 10 trys to guess the word good luck!\n")
 
-print(target_word)
-for letter in guessed_letters:
-    print(letter, end=" ")
+while ((attempts < 10) or (guessed_letters == target_word)):
+    print(target_word)
+    for letter in guessed_letters:
+        print(letter, end=" ")
 
 
-user_input = input("\n\nGuess a letter: ").lower()
+    user_input = input("\n\nGuess a letter: ").lower()
 
-if (user_input in target_word):
-    i = 0
-    for letter in target_word:
-        if user_input == target_word[i]:
-            guessed_letters[i] = user_input
+    if (user_input in target_word):
+        i = 0
+        for letter in target_word:
+            if user_input == target_word[i]:
+                guessed_letters[i] = user_input
 
-        i += 1
+            i += 1
 
+    else:
+        print ("\n Wrong! \n")
+
+    attempts += 1
+
+    print(guessed_letters)
+
+if (guessed_letters == target_word):
+    print("Congratulations!")
 else:
-    print ("\n Wrong! \n")
-
-print(guessed_letters)
+    print("Better luck next time!")
