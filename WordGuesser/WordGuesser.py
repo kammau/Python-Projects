02f87ab30498e20aa9b155d14561af1305856585
Kamryn Smith <programersign@gmail.com>
2026-08-25 16:08:59 -0700

    Adding comments

--- WordGuesser/WordGuesser.py
+++ WordGuesser/WordGuesser.py
@@ -4,22 +4,32 @@
     Program Description: 
 """
 
+# Module to give the program a method to select a random word from words list
 import random
 
 # 10 random words that I generated from the internet LOL
-words = ["Pencil", "Ocean", "Guitar", "Cloud", "Window", "Apple", "Tiger", "House", "River", "Book"]
-target_word = list(random.choice(words).lower())
+words = ["pencil", "ocean", "guitar", "cloud", "window", "apple", "tiger", "house", "river", "book"]
 
+# Stores a randomly chosen word from words list using random's .choice method
+target_word = random.choice(words)
+
+# Welcoming message + getting user's name
 print("--- Word Guessing Game ---")
 name = input("\nWhat is your name player? ")
 print(f"Welcome {name}, to the Word Guessing Game!")
 print("\nYou will have 10 attempts to guess each character in the word. Good luck!\n")
 
+# Store's wrong attempts left before user looses
 attempts = 10
+# Store's each character/letter the user guesses
 guessed_chars = ""
+# Store's the number of characters/letters the user has guessed correctly
 score = 0
 
+# --- Main Game Loop ---
+# While the user hasn't run out of attempts
 while (attempts != 0):
+    # Reset score to 0 
     score = 0
 
     for char in target_word:
@@ -39,7 +49,7 @@ while (attempts != 0):
     #     print(f"\nThe word was: {"".join(target_word)}")
     #     break
 
-    users_guess = input("\n\nGuess a character: ")
+    users_guess = input("\n\nGuess a character: ").lower()
 
     if (len(users_guess) != 1):
         print("\nPlease enter a valid character!\n")
@@ -53,11 +63,11 @@ while (attempts != 0):
 
     if (users_guess not in target_word):
         attempts -= 1
-        
+
         print("\nWrong!")
         print(f"\nYou have {attempts} more attempts!\n")
 
-
-print("\n\nYou loose! Better luck next time...")
-print(f"\nThe word was: {"".join(target_word)}")
+        if (attempts == 0):
+            print("\n\nYou loose! Better luck next time...")
+            print(f"\nThe word was: {"".join(target_word)}")
         
